a897935d5974c118ba01d35e54a29220d12f9793
ogabek <quvvatullayev@gmail.com>
2023-10-24 14:35:55 +0500

    bus is created model

--- bus/models.py
+++ bus/models.py
@@ -2,10 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-class Bus(models.Model):
+class Buss(models.Model):
     busnumber = models.CharField(max_length=50)
     bussum = models.PositiveIntegerField(default=1)
     timelimte = models.TimeField()
 
     def __str__(self) -> str:
         return self.busnumber
+    
+class Bus(models.Model):
+    driver = models.CharField(max_length=30)
+    busid = models.CharField(max_length=30)
+    img = models.ImageField(upload_to='./driverimg')
+    bus = models.ForeignKey(Buss, on_delete=models.CASCADE)
+
+    let = models.IntegerField()
+    long = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.driver
