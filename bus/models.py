0fe3a342f2c735a955cfa3848fb905b33ff40ee7
ogabek <quvvatullayev@gmail.com>
2023-10-24 14:27:36 +0500

    bus is create model

--- bus/models.py
+++ bus/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Bus(models.Model):
+    busnumber = models.CharField(max_length=50)
+    bussum = models.PositiveIntegerField(default=1)
+    timelimte = models.TimeField()
+
+    def __str__(self) -> str:
+        return self.busnumber
