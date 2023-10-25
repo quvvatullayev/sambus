c2ac0f1c4907fd70ea805d77c7e947f3f9adf5a7
ogabek <quvvatullayev@gmail.com>
2023-10-25 15:50:26 +0500

    route views are add url

--- bus/urls.py
+++ bus/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.buss import BussDetilView, BussListCreateView
 from .views.bus import BusListCreateView, BusDetilView
+from .views.route import RouteListCreateView, RouteDetilView
 
 urlpatterns = [
     # Buss views
@@ -9,5 +10,9 @@ urlpatterns = [
 
     # Bus views
     path('bus/lsit/', BusListCreateView.as_view()),
-    path('bus/<int:pk>/', BusDetilView.as_view())
+    path('bus/<int:pk>/', BusDetilView.as_view()),
+
+    # Route views
+    path('route/lsit/', RouteListCreateView.as_view()),
+    path('route/<int:pk>/', RouteDetilView.as_view()),
 ]
