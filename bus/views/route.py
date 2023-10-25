d988f10fbb8ef9537139a53b982cd42aa4c10170
ogabek <quvvatullayev@gmail.com>
2023-10-25 15:46:34 +0500

    route list create view is create

---
+++ bus/views/route.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+from ..serializer import Route, RouteSerializer
+
+class RouteListCreateView(generics.ListCreateAPIView):
+    request = Route.objects.all()
+    serializer_class = RouteSerializer
