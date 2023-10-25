2f0f3025c2f7de3e2a2970bbe7260c074d44eaea
ogabek <quvvatullayev@gmail.com>
2023-10-25 16:02:42 +0500

    stations views are add urls file

--- bus/urls.py
+++ bus/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views.buss import BussDetilView, BussListCreateView
 from .views.bus import BusListCreateView, BusDetilView
 from .views.route import RouteListCreateView, RouteDetilView
+from .views.station import StationListCreateView, StationDetilView
 
 urlpatterns = [
     # Buss views
@@ -15,4 +16,8 @@ urlpatterns = [
     # Route views
     path('route/lsit/', RouteListCreateView.as_view()),
     path('route/<int:pk>/', RouteDetilView.as_view()),
+
+    # Station views
+    path('station/list/', StationListCreateView.as_view()),
+    path('station/<int:pk>/', StationDetilView.as_view()),
 ]
