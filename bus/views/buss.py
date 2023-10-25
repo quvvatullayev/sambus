2e07f31755b50e2cb4a09ef677ff3f7cae6b44e2
ogabek <quvvatullayev@gmail.com>
2023-10-25 13:39:04 +0500

    buss list create view is created

---
+++ bus/views/buss.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+from serializer import Buss, BussSerializer
+
+class BussListCreateView(generics.ListCreateAPIView):
+    queryset = Buss.objects.all()
+    serializer_class = BussSerializer
+    
