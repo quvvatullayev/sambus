77336fc9593952f7dd333e0b6a2723550bbdf28c
ogabek <quvvatullayev@gmail.com>
2023-10-31 15:34:21 +0500

    user list create view is created

---
+++ user/views/userview.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+from ..serializer import UserModel, UserSerializer
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserSerializer
