3abab533ea42386124dab3e708b13af6c9cf182e
ogabek <quvvatullayev@gmail.com>
2023-10-31 14:08:41 +0500

    buss detil view is fix

--- bus/views/buss.py
+++ bus/views/buss.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.request import Request
 from rest_framework.response import Response
-from ..serializer import Buss, BussSerializer
+from ..serializer import Buss, BussSerializer, Route, RouteSerializer
 
 class BussListCreateView(generics.ListCreateAPIView):
     queryset = Buss.objects.all()
@@ -11,3 +11,16 @@ class BussListCreateView(generics.ListCreateAPIView):
 class BussDetilView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Buss.objects.all()
     serializer_class = BussSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        buss_obj = Buss.objects.get(pk = kwargs['pk'])
+        buss = BussSerializer(buss_obj).data
+
+        route_obj = Route.objects.get(buss = buss['id'])
+        route = RouteSerializer(route_obj, many = False).data
+
+        data = {
+            'buss':buss,
+            'route':route,
+        }
+        return Response(data=data)
