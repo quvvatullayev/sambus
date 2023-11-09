16edb73c00541ce2a48ee472e13fbf9845c441dd
ogabek <quvvatullayev@gmail.com>
2023-11-09 14:57:20 +0500

    corsheaders are add settings

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # global app
     "rest_framework",
     "drf_yasg",
+    "corsheaders",
     
     # local app
     'bus',
@@ -51,6 +52,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
