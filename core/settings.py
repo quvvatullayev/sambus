293c05877948c8926023e671455cce9954b5114e
ogabek <quvvatullayev@gmail.com>
2023-11-09 15:26:36 +0500

    static files add

--- core/settings.py
+++ core/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/ 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
