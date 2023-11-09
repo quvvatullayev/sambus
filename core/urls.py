56200927095b81fbd3366ca351b705ce1691d852
ogabek <quvvatullayev@gmail.com>
2023-11-09 16:14:56 +0500

    fix

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,10 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -24,4 +28,4 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('', include('bus.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
