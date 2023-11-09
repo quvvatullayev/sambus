1fa9af95d102af27e2d1ac1dd2117e4e2736e6db
ogabek <quvvatullayev@gmail.com>
2023-11-09 14:00:08 +0500

    user model is fix

--- user/models.py
+++ user/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from bus.models import Station
 
 class UserModel(User):
-    location = models.JSONField()
-    my_bus_location = models.JSONField()
+    location = models.JSONField(blank=True, null=True)
+    my_bus_location = models.ForeignKey(Station, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.username
