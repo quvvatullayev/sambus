607cb1e84c85391b6ed797b25f073dddb997e630
ogabek <quvvatullayev@gmail.com>
2023-11-15 14:20:03 +0500

    user stations is add urls

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from views.userview import UserListCreateView, UserDetil
+from views.userview import UserListCreateView, UserDetil, UserFindNearestStation
 
 urlpatterns = [
     path('list/', UserListCreateView.as_view()),
     path('<int:pk>/', UserDetil.as_view()),
+
+    path('station/<int:pk>/', UserFindNearestStation.as_view())
 ]
