795a510c3e4c16ff0bf7d490e792dee121882763
ogabek <quvvatullayev@gmail.com>
2024-04-30 22:13:13 +0500

    m

--- core/settings.py
+++ core/settings.py
@@ -60,12 +60,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
-STATIC_ROOT = '/home/django/django_project/django_project/static'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static") 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # STATIC_ROOT = BASE_DIR ,'static'  # Yangi direktoriya nomi
 
