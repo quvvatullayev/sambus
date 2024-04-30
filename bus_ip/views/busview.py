c6efb313800a53b57fa2c0a5db79aad6ab3e4580
ogabek <quvvatullayev@gmail.com>
2024-04-30 15:51:37 +0500

    bus detile is fixed

--- bus_ip/views/busview.py
+++ bus_ip/views/busview.py
@@ -11,3 +11,12 @@ class BusRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BusModel.objects.all()
     serializer_class = BusSerializer
 
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object()  # Get the requested BusModel instance
+        bus_stops = instance.busstopmodel_set.all()  # Get all BusStopModel instances related to this BusModel
+        serializer = self.get_serializer(instance)
+        return Response({
+            'bus': serializer.data,
+            'bus_stops': [stop.busstop_name for stop in bus_stops]  # Serialize bus stops
+        })
+
