ff9a5c87a127a9da83684669e8a24a157845f3f5
ogabek <quvvatullayev@gmail.com>
2024-05-03 12:01:37 +0500

    fix

--- bus_ip/urls.py
+++ bus_ip/urls.py
@@ -31,19 +31,19 @@ urlpatterns = [
     path('user/logout/', LogoutUser.as_view()),
 
     path('busstop/', BusStopCreateView.as_view(), name='busstop'),
-    path('busstops/', BusStopListView.as_view(), name='busstop'),
+    path('busstop/list/', BusStopListView.as_view(), name='busstop'),
     path('busstop/<int:pk>/', BusStopRetrieveView.as_view()),
     path('busstop/<int:pk>/', BusStopDestroyView.as_view()),
     path('busstop/<int:pk>/', BusStopUpdateView.as_view()),
     
-    path('buss/', BusListView.as_view()),
+    path('bus/list/', BusListView.as_view()),
     path('bus/', BusCreateView.as_view()),
     path('bus/<int:pk>/', BusUpdeteView.as_view()),
     path('bus/<int:pk>/', BusDestroyView.as_view()),
     path('bus/<int:pk>/', BusRetrieveView.as_view()),
 
 
-    path('users/', UserListView.as_view()),
+    path('user/list/', UserListView.as_view()),
     path('user/', UserCreateView.as_view()),
     path('user/<int:pk>/', UserDestroyView.as_view()),
     path('user/<int:pk>/', UserUpdateView.as_view()),
