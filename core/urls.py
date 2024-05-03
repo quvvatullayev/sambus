2aa23f771229afd04608c6303ea8782757b3a2b1
ogabek <quvvatullayev@gmail.com>
2024-05-03 12:06:05 +0500

    fix

--- core/urls.py
+++ core/urls.py
@@ -25,10 +25,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
-    path('bus_ip/', include('bus_ip.urls')),
-    path('auth/', include('rest_framework.urls'), name='auth'),
+    path('swagger/',         schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/',           schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/',           admin.site.urls),
+    path('bus_ip/',          include('bus_ip.urls')),
+    path('auth/',            include('rest_framework.urls'), name='auth'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
